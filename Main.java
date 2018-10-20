71a66029ec4dcd9e0b4d90750ac8450949d521ba
Chima Ani <ekperederek@gmail.com>
2018-10-20 19:42:58 -0400

    Updated GUI
    Location gets file path

--- Main.java
+++ Main.java
@@ -14,30 +14,30 @@ import javafx.stage.Stage;
  * @author chima
  */
 public class Main extends Application {
-    @Override
-    public void start(Stage primarystage) {
-        Parent root;
+	@Override
+	public void start(Stage primarystage) {
+		Parent root;
 		try {
-			
+
 			root = FXMLLoader.load(getClass().getResource("/application/Main.fxml"));
 			Scene scene = new Scene(root);
-	        
-	        primarystage.setScene(scene);
-	        primarystage.show();
+
+			primarystage.setScene(scene);
+			primarystage.show();
 		}
 		catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
-        
-    }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
+
+	}
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
     
 }
--- MainController.java
+++ MainController.java
@@ -3,6 +3,8 @@ package application;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,88 +13,78 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
 /**
  *
  * @author chima
  */
 public class MainController implements Initializable {
-    
-    @FXML
-    private ListView listview;
-    private ListView fpath;
-    
-    @FXML
-    private Button btnfc;
-    
-    
-    
-    
-    
-   
-    //file chooser method
-    public void Button1Action(ActionEvent event) throws InterruptedException {
-        FileChooser fc = new FileChooser();
-        File seletedFile = fc.showOpenDialog(null);
-            if (seletedFile != null) {
-          
-            Thread t1= new Thread () {
-            	public void run() {
-            
-            	listview.getItems().add(seletedFile.getName());
-            	}};
-            	t1.start();
-         //location need to be fixed
-            	/*  	Thread t2 = new Thread() {
-            		
-            		public void run() {
-            		
-            			fpath.getItems().add(seletedFile.getAbsolutePath());
-            		}};
-            		t1.join();
-           */
-            	 }else {
-                System.out.println("File is not valid");
-            }           
-                
-    }   
-    //password field
-    String password = "test";
-    @FXML
-    private PasswordField pw;
-    @FXML
-    private Button pbtn;
-    
-    //validates the password 
-    @FXML
-    private void validate(ActionEvent event) {
-    String Password=pw.getText();
-    if(password.isEmpty()) {
+
+	@FXML
+	private ListView listview;
+
+	@FXML
+	private Button btnfc;
+
+	@FXML
+	private TextArea location;
+
+	//file chooser method
+	public void Button1Action(ActionEvent event) throws InterruptedException {
+		FileChooser fc = new FileChooser();
+		
+		//opens window to select file
+		File seletedFile = fc.showOpenDialog(null);
+		
+		// ObservableList listofItems = listview.getItems();   
+		if (seletedFile != null) {
+
+			// gets selected file and name
+			listview.getItems().add(seletedFile.getName());
+
+			// gets the absolute path of selected file
+			location.setText(seletedFile.getAbsolutePath());    	 
+
+		}else {
+			System.out.println("File is not valid");
+		}
+
+	}   
+
+	//password field 
+	String password = "test";
+	@FXML
+	private PasswordField pw;
+	@FXML
+	private Button pbtn;
+
+	//validates the password 
+	@FXML
+	private void validate(ActionEvent event) {
+		String Password=pw.getText();
+		/*   if(password.isEmpty()) {
     	Alert alert = new Alert(AlertType.ERROR);
     	alert.setContentText("Incorrect Password");
     	alert.showAndWait();
-    }else {
-    	if(password.equals(pw)) {
-    		Alert alert = new Alert(AlertType.INFORMATION);
-        	alert.setHeaderText(null);
-    		alert.setContentText("Success");
-        	alert.showAndWait();
-    	}else {
-     
+    }else {*/
+		if(password.equals(pw)) {
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setHeaderText(null);
+			alert.setContentText("Success");
+			alert.showAndWait();
+			/*}else {
+
             		Alert alert = new Alert(AlertType.INFORMATION);
                 	alert.setHeaderText(null);
             		alert.setContentText("Incorrect");
-                	alert.showAndWait();
-    	}
-                 
-    
-   
-   
-    }
-    }
-    
+                	alert.showAndWait(); */
+		}
+
+	}
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}}
