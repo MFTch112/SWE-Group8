78180435525351e70da7a3d509d07abbbb1cd5c0
Chima Ani <ekperederek@gmail.com>
2018-10-20 00:08:09 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+/**
+ *
+ * @author chima
+ */
+public class Main extends Application {
+    @Override
+    public void start(Stage primarystage) {
+        Parent root;
+		try {
+			
+			root = FXMLLoader.load(getClass().getResource("/application/Main.fxml"));
+			Scene scene = new Scene(root);
+	        
+	        primarystage.setScene(scene);
+	        primarystage.show();
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
